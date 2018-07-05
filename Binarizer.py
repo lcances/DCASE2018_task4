35714c2248c53b7ef7950f4e444b02746439f7c0
leocancse <leocances@gmail.com>
2018-07-05 17:57:16 +0200

    Binarizer singleton with optimization methods. Binarization can work on 2 or 3 dimension np.array

--- Binarizer.py
+++ Binarizer.py
@@ -1,20 +1,20 @@
 """
-TODO
+Allow the binarization of the prediction results using global thresholds or optimized one.
+These thresholds are set to 0.5 as default and can be optimized using AUC score
 """
-__author__ = "Léo Cances"
-__copyright__ = ""
-__credits__ = ["Léo Cances"]
-__license__ = "GPL"
-__version__ = "1.0.0"
-__maintainer__ = "Léo Cances"
-__email__ = "leocances@gmail.com"
-__status__ = "Production"
-
 from datasetGenerator import DCASE2018
 import numpy as np
 import copy
+import sys
 
-class Binarizer:
+
+class Binarizer(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Binarizer, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
         self.thresholds = dict()
         self.optimized = False
@@ -46,17 +46,89 @@ class Binarizer:
 
     def binarize(self, predictionResult: np.array) -> np.array:
         """
-        Binarize the prediction results given using the defines thresholds, Each column represent one class.
-        :param predictionResult: 2-dimensions numpy array not binarized
-        :return: 2-dimension numpy array representing the binarized prediction
+        Binarize the prediction results given using the defines thresholds, Can work with global prediction and
+        temporal prediction
+        :param predictionResult: 2 or 3 dimensions numpy array not binarized
+        :return: 2 or 3 dimension numpy array representing the binarized prediction
         """
+        if len(predictionResult.shape) == 2:
+            return self.__globalBinarization(predictionResult)
+
+        elif len(predictionResult.shape) == 3:
+            return self.__temporalBinarization(predictionResult)
+
+        else:
+            print("Can't binarize on a array of dimension different that 2 or 3")
+            sys.exit(1)
+
+
+    def __globalBinarization(self, predictionResult: np.array) -> np.array:
         output = []
         mappedScore = np.nan_to_num([self.thresholds[key] for key in self.thresholds])
         for i in range(len(predictionResult)):
             line = copy.copy(predictionResult[i])
 
             line[line > mappedScore] = 1
-            line[line < mappedScore] = 0
+            line[line <= mappedScore] = 0
             output.append(line)
 
         return np.array(output)
+
+    def __temporalBinarization(self, temporalPrediction: np.array) -> np.array:
+        output = []
+        mappedScore = np.nan_to_num([self.thresholds[key] for key in self.thresholds])
+
+        for clip in temporalPrediction:
+            bin = []
+
+            for i in range(len(clip)):
+                line = copy.copy(clip[i])
+
+                line[line > mappedScore] = 1
+                line[line <= mappedScore] = 0
+                bin.append(line)
+
+            output.append(bin)
+
+        return np.array(output)
+
+if __name__=="__main__":
+    import random
+    # create fake data (global prediction)
+    def fakeGlobalPrediction():
+        prediction = []
+        for i in range(1000):
+            score = [random.random() for i in range(10)]
+            prediction.append(score)
+
+        prediction = np.array(prediction)
+        print(prediction.shape)
+        print(prediction)
+
+        b = Binarizer()
+        binPrediction = b.binarize(prediction)
+        print(binPrediction)
+        print(binPrediction.shape)
+
+    # create fake data (temporal prediction)
+    def fakeTemporalPrediction():
+        prediction = []
+        for i in range(1000):
+            clip = []
+            for j in range(500):
+                score = [random.random() for k in range(10)]
+                clip.append(score)
+            prediction.append(clip)
+
+        prediction = np.array(prediction)
+        print(prediction)
+        print(prediction.shape)
+
+        b = Binarizer()
+        binPrediction = b.binarize(prediction)
+        print(binPrediction)
+        print(binPrediction.shape)
+
+    fakeTemporalPrediction()
+
+
